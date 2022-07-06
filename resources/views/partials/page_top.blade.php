8a5fbe8a5cf12a80bcf9dea7235b736d7a06c591
Ulugbek Yusupxodjayev <ulugbek.yu@gmail.com>
2022-07-06 15:53:03 +0500

    Style updated

--- resources/views/partials/page_top.blade.php
+++ resources/views/partials/page_top.blade.php
@@ -4,6 +4,9 @@
             <div class="col-lg-8 offset-lg-2">
                 <h1 class="text-secondary">{{ $title ?? '' }}</h1>
                 @include('partials.breadcrumbs')
+                @if (!empty($description))
+                    <p class="mt-4 mb-0">{{ $description }}</p>
+                @endif
             </div>
         </div>
     </div>
